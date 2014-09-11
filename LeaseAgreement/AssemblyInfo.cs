8df81e489962dced7d1eeab807805caaceaeaa7c
Andrey Gankov <gav@qsolution.ru>
2014-09-11 15:27:08 +0400

    Диалог About из библиотеки.

--- LeaseAgreement/AssemblyInfo.cs
+++ LeaseAgreement/AssemblyInfo.cs
@@ -1,17 +1,24 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using QSProjectsLib;
 
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
 [assembly: AssemblyTitle("QS: Договора аренды")]
-[assembly: AssemblyDescription("Учет договоров аренды")]
+[assembly: AssemblyDescription("Программа позволяет управлять договорами аренды.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Quality Solution")]
 [assembly: AssemblyProduct("")]
 [assembly: AssemblyCopyright("Quality Solution 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: AssemblyLogoIcon("LeaseAgreement.icons.logo.png")]
+[assembly: AssemblySupport("", true, "MySQL")]
+[assembly: AssemblyAppWebsite("http://www.qsolution.ru/")]
+
+[assembly: AssemblyAuthor("Ганьков Андрей <gav@qsolution.ru>")]
+
 
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
