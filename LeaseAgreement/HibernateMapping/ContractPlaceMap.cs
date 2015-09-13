f4cf725ee9cbff1de86ca908dc245bdfccb3e647
Andrey Gankov <gav@qsolution.ru>
2015-09-13 11:55:39 +0300

    Добавлен маппинг аренды места.

---
+++ LeaseAgreement/HibernateMapping/ContractPlaceMap.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FluentNHibernate.Mapping;
+using LeaseAgreement.Domain;
+
+namespace LeaseAgreement.HMap
+{
+	public class ContractPlaceMap : ClassMap<ContractPlace>
+	{
+		public ContractPlaceMap ()
+		{
+			Table ("contract_places");
+			Not.LazyLoad ();
+
+			Id (x => x.Id).Column ("id").GeneratedBy.Native ();
+			Map (x => x.StartDate).Column ("start_date");
+			Map (x => x.EndDate).Column ("end_date");
+			References (x => x.Contract).Column ("contract_id");
+			References (x => x.Place).Column ("place_id");
+		}
+	}
+}
+
