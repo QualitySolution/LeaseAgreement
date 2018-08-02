3aa0d3c153134c177a3dfcb24527eb6d848dd535
Andrey Gankov <gav@qsolution.ru>
2018-08-02 15:55:46 +0300

    Добавлен Gamma вариант виджета CompanyName

---
+++ LeaseAgreement/Widgets/yCompanyName.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq.Expressions;
+using Gamma.Binding.Core;
+using QSWidgetLib;
+
+namespace Gamma.Widgets
+{
+	[System.ComponentModel.ToolboxItem (true)]
+	[System.ComponentModel.Category ("Gamma")]
+	public class yCompanyName : CompanyName
+	{
+		public BindingControler<yCompanyName> Binding { get; private set;}
+
+		public yCompanyName ()
+		{
+			Binding = new BindingControler<yCompanyName> (this, new Expression<Func<yCompanyName, object>>[] {
+				(w => w.Text)
+			});
+		}
+
+		protected override void OnChanged ()
+		{
+			Binding.FireChange (w => w.Text);
+			base.OnChanged ();
+		}
+	}
+}
+
